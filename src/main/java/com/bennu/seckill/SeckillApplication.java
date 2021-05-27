8a8fdf40e6fe1ee06f1c2519f2e142ff4c0f098d
bennu5 <wang.mingming@hotmail.com>
2021-05-27 21:18:40 +0800

    Rabbit 及其他逻辑转换

--- src/main/java/com/bennu/seckill/SeckillApplication.java
+++ src/main/java/com/bennu/seckill/SeckillApplication.java
@@ -1,8 +1,14 @@
 package com.bennu.seckill;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * @author Bennu
+ */
 @SpringBootApplication
 public class SeckillApplication {
 
@@ -10,4 +16,8 @@ public class SeckillApplication {
         SpringApplication.run(SeckillApplication.class, args);
     }
 
+    @Bean
+    MessageConverter createMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
