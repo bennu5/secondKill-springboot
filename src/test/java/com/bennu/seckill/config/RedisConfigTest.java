d7952989b6bc3303d032034b77d17d7a21cb7cf2
bennu5 <wang.mingming@hotmail.com>
2021-10-02 22:02:36 +0800

    中间件配置

--- src/test/java/com/bennu/seckill/config/RedisConfigTest.java
+++ src/test/java/com/bennu/seckill/config/RedisConfigTest.java
@@ -8,8 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RedisConfigTest {
     @Test
-    private void connect(){
-        RedisClient redisClient = RedisClient.create("redis://1Jdfdsj#@$DFSWdsd@#%sdfsd#fd@82.157.1.224:6379/");
+    void connect(){
+        RedisClient redisClient = RedisClient.create("redis://1Jdf3dsjDF2SW5ds7ds9df3sd5fd@82.157.1.224:6379/0");
         StatefulRedisConnection<String, String> connection = redisClient.connect();
+        System.out.println("Connected to Redis");
+        connection.sync().set("key", "Hello World");
+        connection.close();
+        redisClient.shutdown();
+    }
+
+    @Test
+    void getTest(){
+        RedisClient redisClient = RedisClient.create("redis://1Jdf3dsjDF2SW5ds7ds9df3sd5fd@82.157.1.224:6379/0");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        System.out.println("Connected to Redis");
+        String value = connection.sync().get("key");
+        System.out.println("Get value from redis:" + value);
+        connection.close();
+        redisClient.shutdown();
     }
 }
